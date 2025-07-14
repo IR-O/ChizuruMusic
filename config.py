752d89fa88ae0d0792f5c27c9221801e6fceba2a
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2025-07-15 02:31:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "28542531"))
 API_HASH = getenv("API_HASH", "9f4889cd2437d72ede20428c07a909be")
-BOT_TOKEN = getenv("BOT_TOKEN", "7953139932:AAFLRe5L0HSdbboNYDKzchLEBFQx4vJrIZA")
+BOT_TOKEN = getenv("BOT_TOKEN", "5964346248:AAGtWqikxHyNjT9qKPMpEeN7rSqhy1XWWwc")
 OWNER_ID = int(getenv("OWNER_ID", "6045293810"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6045293810").split()))
-MONGO_URL = getenv("MONGO_URL", "mongodb+srv://nishamusic:Nothing0000@nishamusicbot.dg20jss.mongodb.net/?retryWrites=true&w=majority&appName=NishaMusicBot")
-SESSION_STRING = getenv("SESSION_STRING", "BQAf70YAVf30uzzX36VSAjwLDGMJq1hF6vXK0CNsnW_9tZcFu_c5FK13yVVKMHUNBFnX3GhHzNRkE4E-GmARk6sK_K8n9kEj_TEpTe5rPDkqViEqO4K-3MQiijl0-84kgPhrG9TTQzHuBqhJ6Nn0SDHe0QaE1_3EP3O3AJgRHZdkLueW9iqHXaMjrE2GA06RWncL98r5J90wIjPa2zrdYKwi9stN-RgqFexLuXMT2n0PEG-fxXnKdQXkV_C98UJjdT6y5KinDpSCDFn0D2Ctwdd69R5LIE7qm5Ufs4Zk2x5KhBfMU5JO_75r-OWAlME-okodxdd35tS3XNYHuEIHvX0dpS23agAAAAGExJmlAA")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://String:iro@string.bfl5lcm.mongodb.net/?retryWrites=true&w=majority&appName=String")
+SESSION_STRING = getenv("SESSION_STRING", "BQDEFKsAgiokuBQL9u39ZqrvaCaQO3LKSggOy37pSrHZT5zVqgjq142NHplMweczk3QX9d7zyZtJRnkbAf68dI1UwZL6Zt8E-easqg2itztgxj1x9va6IZsqNUh9L6g8vo56oMeRC9zQ2B3ZIKQUAr-SQmbwKy4yaoZNV2dLrOa0CUC_F1gDWEcLdk-hdoUsQIfEeKVpVKJz8Qgj9YEd8YNgHjbt-TFE5WGcLhV12kjkCw8PCsx1sbALnoNk22TVrhc8HJQRuB5gdJxJeer8a1B4JBrSso6B84LPI0SjAHgu-xCiDI8Mi3h4YhM4c5EN-ajrSJZczpCYkp633E6eYan31cr2FgAAAAHmorCNAA")
 
 
