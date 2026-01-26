e9687fd7a52910606141f90f1efeee28fd563f0d
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2026-01-26 21:24:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "28542531"))
 API_HASH = getenv("API_HASH", "9f4889cd2437d72ede20428c07a909be")
-BOT_TOKEN = getenv("BOT_TOKEN", "6365132039:AAF48I0KgZe4cyHmhMiRx_K634u6BEKApDQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "8319239223:AAFlIgIjlEo3rZD3shqvHk3F_0kkf-DZ1OU")
 OWNER_ID = int(getenv("OWNER_ID", "6045293810"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6045293810").split()))
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://String:iro@string.bfl5lcm.mongodb.net/?retryWrites=true&w=majority&appName=String")
-SESSION_STRING = getenv("SESSION_STRING", "BQDEFKsAgiokuBQL9u39ZqrvaCaQO3LKSggOy37pSrHZT5zVqgjq142NHplMweczk3QX9d7zyZtJRnkbAf68dI1UwZL6Zt8E-easqg2itztgxj1x9va6IZsqNUh9L6g8vo56oMeRC9zQ2B3ZIKQUAr-SQmbwKy4yaoZNV2dLrOa0CUC_F1gDWEcLdk-hdoUsQIfEeKVpVKJz8Qgj9YEd8YNgHjbt-TFE5WGcLhV12kjkCw8PCsx1sbALnoNk22TVrhc8HJQRuB5gdJxJeer8a1B4JBrSso6B84LPI0SjAHgu-xCiDI8Mi3h4YhM4c5EN-ajrSJZczpCYkp633E6eYan31cr2FgAAAAHmorCNAA")
+SESSION_STRING = getenv("SESSION_STRING", "BQFVD0wAfL6rlpVParik224hq_VRptmdycjrhV788nRMGdS_YYt6O25TkJwpbjmgKyfrDAnXnDCrVIJwPjhbuLelWp8UK8voOklIm0CFwlzSnRTxRzWQcJRZTRhuRelr14r5OIw9CqNqCPddJ-N6LkjD9JnOk6TNBv5ubY7X0xmNRJzs_NN4lPsjqpzXV-1LPCciO54fuC2P6TImr81HHs6Y6eX7J8NseA1qaTRbzogOVbnXyWIknboV0XxY5VsxdLl6vdpQ0SiiJYwBFAeLMHd4UI-GtojE6zp71CSyuYCpRbsal66ON6c_qFjKYznpIcjQw1Jhe9gspQYNK3DGTPvZge1NjAAAAAFKEGWjAA")
 
 
